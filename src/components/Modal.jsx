c4d0f86725378c5ee090837e6e719d054c7ce455
vickyruud <aprajit.k@gmail.com>
2022-06-22 12:52:45 -0400

    modify modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,8 +8,7 @@ const Modal = ({modalType, showModal, setShowModal, login, register}) => {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             {modalType === 'login' && <LoginForm setShowModal={setShowModal} login={login} />}
             {modalType === 'sign-up' && <SignUp setShowModal={setShowModal} register={register} />}
-            {modalType === 'new-comment' && <NewComment setShowModal={setShowModal} />}
-            {modalType === 'new-recipe' && <NewRecipe setShowModal={setShowModal} />}
+            
 
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
