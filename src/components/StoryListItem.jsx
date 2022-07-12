d123bb22393c0f987f0c937a43a20f33236c12fa
vickyruud <aprajit.k@gmail.com>
2022-07-11 22:33:21 -0400

    conditionally style upvote arrow

--- src/components/StoryListItem.jsx
+++ src/components/StoryListItem.jsx
@@ -1,28 +1,33 @@
 import { useContext } from "react";
 import { AiOutlineArrowDown } from "react-icons/ai";
-import {FaArrowUp} from "react-icons/fa"
+import { BsShift, BsShiftFill } from "react-icons/bs";
 import { AppContext } from "../App";
 import TimeAgo from "timeago-react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const StoryListItem = ({ story, user }) => {
- 
-
   const { updateStory, loggedUser } = useContext(AppContext);
 
+  const [upVoted, setUpVoted] = useState(false);
+
   const handleUpVote = () => {
+    //checks if user is logged in.
     if (loggedUser) {
+      //records the ID of the user who is logged in.
       const id = loggedUser.id.toString();
 
+      //has the user up voted the post already?
       if (story.usersUpVoted.includes(id)) {
+        //create new instances removing the current user from the already voted list
         const newUsersUpVoted = story.usersUpVoted.filter(
           (user) => user !== id
         );
         const newUsersDownVoted = story.usersDownVoted.filter(
           (user) => user !== id
-        );        
-        
-        const newVotes = story.votes - 1        
-        
+        );
+
+        const newVotes = story.votes - 1;
 
         const newStory = {
           ...story,
@@ -30,32 +35,29 @@ const StoryListItem = ({ story, user }) => {
           usersUpVoted: newUsersUpVoted,
           usersDownVoted: newUsersDownVoted,
         };
+        //updates the database
         updateStory(newStory);
-      
-
       } else {
-
-          let votes;
+        let votes;
+        //checks if user has already down voted.
 
         if (story.usersDownVoted.includes(id)) {
-           votes = story.votes + 2
-          
+          votes = story.votes + 2;
         } else {
-          votes = story.votes + 1
+          votes = story.votes + 1;
         }
-         const newUsersDownVoted = story.usersDownVoted.filter(
+        //create new instances removing the current user from the already voted list
+        const newUsersDownVoted = story.usersDownVoted.filter(
           (user) => user !== id
-        );  
-        
+        );
+
         const newStory = {
           ...story,
           votes: votes,
           usersUpVoted: [...story.usersUpVoted, id],
-          usersDownVoted: newUsersDownVoted
+          usersDownVoted: newUsersDownVoted,
         };
         updateStory(newStory);
-        
-
       }
     } else {
       console.log("please login");
@@ -72,11 +74,9 @@ const StoryListItem = ({ story, user }) => {
         );
         const newUsersDownVoted = story.usersDownVoted.filter(
           (user) => user !== id
-        );  
+        );
 
         let votes = story.votes + 1;
-
-      
 
         const newStory = {
           ...story,
@@ -87,24 +87,22 @@ const StoryListItem = ({ story, user }) => {
 
         updateStory(newStory);
       } else {
-
-         let votes;
+        let votes;
 
         if (story.usersUpVoted.includes(id)) {
-           votes = story.votes - 2
-          
+          votes = story.votes - 2;
         } else {
-          votes = story.votes - 1
+          votes = story.votes - 1;
         }
 
-         const newUsersUpVoted = story.usersUpVoted.filter(
+        const newUsersUpVoted = story.usersUpVoted.filter(
           (user) => user !== id
         );
         const newStory = {
           ...story,
           votes: votes,
           usersDownVoted: [...story.usersDownVoted, id],
-          usersUpVoted: newUsersUpVoted
+          usersUpVoted: newUsersUpVoted,
         };
         updateStory(newStory);
       }
@@ -113,12 +111,27 @@ const StoryListItem = ({ story, user }) => {
     }
   };
 
+  useEffect(() => {
+    if (loggedUser) {
+      const id = loggedUser.id.toString();
+
+      if (story.usersUpVoted.includes(id)) {
+        setUpVoted(true);
+      } else {
+        setUpVoted(false);
+      }
+    }
+  }, [loggedUser, story.usersUpVoted]);
+
   return (
     <div className="p-4 ">
       <div className=" w-full lg:max-w-full flex flex-row lg:flex border-2 shadow-xl shadow-stone-400 border-gray-400   lg:border-gray-400 ">
         <div className=" pl-4 pr-4 flex flex-col justify-center  items-center ">
-          <button className="text-red-400 hover:text-red-800" onClick={handleUpVote}>
-            <FaArrowUp/>             
+          <button
+            className="text-red-400 hover:text-[#7d1a1a] hover:font-bold"
+            onClick={handleUpVote}
+          >
+            {upVoted ? <BsShiftFill/> : <BsShift />}
             
           </button>
           <p className="">{story.votes}</p>
@@ -137,7 +150,7 @@ const StoryListItem = ({ story, user }) => {
             <div className="text-sm">
               <p className="text-gray-900 leading-none font-bold capitalize pb-1">{`${user.first_name}  ${user.last_name}`}</p>
               <p className="text-gray-600font-semibold">
-                <TimeAgo datetime={story.createdAt}/>
+                <TimeAgo datetime={story.createdAt} />
               </p>
             </div>
           </div>
